c153ef1b3853a0ccc364940a18690ff854c0d894
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-08-22 21:59:03 +0200

    Add counter for correct answers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,55 +8,67 @@ interface Question {
 
 const App = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
-  const [questionId, setQuestionId] = useState<number>(0);
+  const [id, setId] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [answerValue, setAnswerValue] = useState<number | undefined>(undefined);
+  const [resultQuiz, setResultQuiz] = useState<number>(0);
 
   useEffect(() => {
     fetch("http://localhost:3001/")
       .then((response) => response.json())
       .then((data) => {
         setQuestions(data.quizQuestionsList);
-        // console.log(data.quizQuestionsList);
         setIsLoading(false);
       })
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(questionId);
-
-  const activeQuestionNumber = questionId + 1;
+  const activeQuestionNumber = id + 1;
   const questionsLenght = questions.length;
+
+  const addToResult = (answerValue: number | undefined) => {
+    if (answerValue !== undefined && answerValue === questions[id].correct) {
+      setResultQuiz((prev) => prev + 1);
+    }
+  };
 
   return (
     <div className="m-1">
       {isLoading ? (
         <h1 className="">Connecting...</h1>
-      ) : questions[questionId] ? (
+      ) : questions[id] ? (
         <div>
           <p>
             Question {activeQuestionNumber} of {questionsLenght}
           </p>
-          <h1 className="">{questions[questionId].name}</h1>
+          <h1 className="">{questions[id].name}</h1>
           <ol className="list-[lower-alpha] list-inside grid grid-cols-2 gap-4 max-w-300">
-            {questions[questionId].answers.map((answer, id) => (
+            {questions[id].answers.map((answer, id) => (
               <button
                 key={id}
                 data-value={id}
                 className="focus:bg-green-500 focus:text-black block cursor-pointer"
-                // onClick={() => console.log(id)}
-              >
+                onClick={() => setAnswerValue(id)}>
                 <li>{answer}</li>
               </button>
             ))}
           </ol>
           <button
             className="bg-stone-700 absolute right-5 cursor-pointer"
-            onClick={() => setQuestionId((prev) => prev + 1)}>
+            onClick={() => {
+              setId((prev) => prev + 1);
+              addToResult(answerValue);
+            }}>
             Next question →
           </button>
         </div>
       ) : (
-        <h1>No more questions</h1>
+        <>
+          <h1>No more questions.</h1>
+          <p>
+            Your result is {resultQuiz} of {questionsLenght} questions
+          </p>
+        </>
       )}
     </div>
   );
