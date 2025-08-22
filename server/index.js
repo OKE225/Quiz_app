f4eac558fe802fb51ea3360f199b1efda8503737
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-08-22 19:03:52 +0200

    Connect to MongoDB using ExpressJS

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const mongo = require("mongodb");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Connect to server!" });
+app.get("/", async (req, res) => {
+  const client = new mongo.MongoClient("mongodb://127.0.0.1:27017");
+
+  try {
+    await client.connect();
+    console.log("Connect to Database");
+
+    const db = client.db("test");
+    const quizQuestions = db.collection("questions");
+
+    const quizQuestionsList = await quizQuestions.find().toArray();
+    res.json({ quizQuestionsList });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  } finally {
+    await client.close();
+  }
 });
 
-app.listen(3001, "localhost");
+app.listen(3001, "localhost", () => {
+  console.log("Server is running on http://localhost:3001");
+});
