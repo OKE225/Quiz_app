f0ca777a2a28c0e65ef7a46f9b3f04c1a5b4dc98
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-08-22 19:04:14 +0200

    Implement simple quiz app functionality for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,63 @@
 import { useEffect, useState } from "react";
 
+interface Question {
+  name: string;
+  answers: string[];
+  correct: number;
+}
+
 const App = () => {
-  const [fetchMessage, setFetchMessage] = useState<string>("");
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questionId, setQuestionId] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetch("http://localhost:3001/")
       .then((response) => response.json())
       .then((data) => {
-        setFetchMessage(data.message);
+        setQuestions(data.quizQuestionsList);
+        // console.log(data.quizQuestionsList);
         setIsLoading(false);
       })
       .catch((error) => console.error(error));
   }, []);
 
+  // for (let i = 0; i <= questions.length; i++) {
+  //   console.log(questions[i]);
+  // }
+
+  console.log(questionId);
+
   return (
-    <>
+    <div className="m-1">
       {isLoading ? (
-        <h1 className="bg-yellow-500 text-3xl font-bold p-4">Connecting...</h1>
+        <h1 className="">Connecting...</h1>
+      ) : questions[questionId] ? (
+        <div>
+          <h1 className="">{questions[questionId].name}</h1>
+          <ol className="list-[lower-alpha] list-inside grid grid-cols-2 gap-4 max-w-300">
+            {questions[questionId].answers.map((answer, id) => (
+              <button
+                key={id}
+                data-value={id}
+                className="focus:bg-green-500 block cursor-pointer"
+                // onClick={() => console.log(id)}
+              >
+                <li>{answer}</li>
+              </button>
+            ))}
+          </ol>
+          <button
+            className="bg-stone-700 absolute right-5 cursor-pointer"
+            onClick={() => setQuestionId((prev) => prev + 1)}>
+            Next question →
+          </button>
+        </div>
       ) : (
-        <h1 className="bg-green-500 text-3xl font-bold p-4">{fetchMessage}</h1>
+        <h1>No more questions</h1>
       )}
-    </>
+    </div>
   );
 };
-
+Implement simple quiz app functionality for testing
 export default App;
